5435e68f7f744fc63c2268f87e617c2329206cad
Michael Lynch <mtlynch@users.noreply.github.com>
2018-10-19 12:16:10 -0500

    Adding line parser for session start line (#5)
    * Adding line parser for session start line
    
    * Changing timestamps to UTC

---
+++ chat_unifier/parsers/trillian/line_parser.py
@@ -0,0 +1,22 @@
+import collections
+import datetime
+from xml.dom import minidom
+
+SessionStartLine = collections.namedtuple(
+    'SessionStartLine',
+    field_names=['timestamp', 'medium', 'sender', 'recipient'])
+
+
+def parse(line):
+    document = minidom.parseString(line)
+    return _parse_session_start(dict(document.childNodes[0].attributes.items()))
+
+
+def _parse_session_start(attributes):
+    timestamp = datetime.datetime.utcfromtimestamp(int(attributes[u'time']))
+
+    return SessionStartLine(
+        timestamp=timestamp,
+        medium=attributes[u'medium'],
+        sender=attributes[u'from'],
+        recipient=attributes[u'to'])
---
+++ tests/parsers/trillian/test_line_parser.py
@@ -0,0 +1,18 @@
+import datetime
+import unittest
+
+from chat_unifier.parsers.trillian import line_parser
+
+
+class LineParserTest(unittest.TestCase):
+
+    def test_parses_valid_session_start_line(self):
+        self.assertEqual(
+            line_parser.SessionStartLine(
+                timestamp=datetime.datetime(2005, 11, 20, 20, 7, 57),
+                medium='AIM',
+                sender='LocalUser456',
+                recipient='RemoteBuddy123'),
+            line_parser.parse(
+                '<session type="start" time="1132517277" medium="AIM" to="RemoteBuddy123" from="LocalUser456"/>'
+            ))
