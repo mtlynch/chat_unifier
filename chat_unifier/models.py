3a4b23512a125d268be16791303bbc7dc894cf00
Michael Lynch <git@mtlynch.io>
2018-10-19 13:54:52 -0500

    Adding models for Session and History

--- chat_unifier/models.py
+++ chat_unifier/models.py
@@ -2,3 +2,7 @@ import collections
 
 Message = collections.namedtuple(
     'Message', field_names=['sender', 'timestamp', 'contents'])
+
+Session = collections.namedtuple('Session', field_names=['messages'])
+
+History = collections.namedtuple('History', field_names=['sessions'])
