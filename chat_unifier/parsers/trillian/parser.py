35b7303258b886fc13addb3be98988411f9bc2f3
Michael Lynch <mtlynch@users.noreply.github.com>
2018-10-19 15:00:06 -0600

    Adding basic parsing for Trillian logs (#10)

--- chat_unifier/parsers/trillian/parser.py
+++ chat_unifier/parsers/trillian/parser.py
@@ -1,5 +1,23 @@
+from chat_unifier import models
+from chat_unifier.parsers.trillian import line_parser
+
+
 class Parser(object):
 
     def parse(self, log_contents):
-        #TODO(mtlynch): Actually parse the contents.
-        raise NotImplementedError('Trillian parser not yet implemented')
+        messages = []
+        lines = [x for x in log_contents.split('\n') if x]
+        for line in lines:
+            parsed_line = line_parser.parse(line)
+            if _is_message_line(parsed_line):
+                messages.append(
+                    models.Message(
+                        sender=parsed_line.sender,
+                        timestamp=parsed_line.timestamp,
+                        contents=parsed_line.contents))
+        return models.History(sessions=[models.Session(messages=messages)])
+
+
+def _is_message_line(parsed_line):
+    return (isinstance(parsed_line, line_parser.OutgoingPrivateMessageLine) or
+            isinstance(parsed_line, line_parser.IncomingPrivateMessageLine))
--- tests/parsers/trillian/test_parser.py
+++ tests/parsers/trillian/test_parser.py
@@ -1,16 +1,29 @@
+import datetime
 import unittest
 
+from chat_unifier import models
 from chat_unifier.parsers.trillian import parser
 
 
-class TrillianTest(unittest.TestCase):
+class ParserTest(unittest.TestCase):
 
     def test_parse_log_with_one_simple_conversation(self):
-        log_contents = r"""
+        self.assertEqual(
+            parser.Parser().parse("""
 <session type="start" time="1132517277" medium="AIM" to="RemoteBuddy123" from="LocalUser456"/>
 <message type="incoming_privateMessage" time="1132517277" medium="AIM" to="LocalUser456" from="RemoteBuddy123" from_display="RemoteBuddy123" text="what%27s%20up"/>
 <message type="outgoing_privateMessage" time="1132522524" medium="AIM" to="RemoteBuddy123" from="LocalUser456" from_display="Me" text="nm"/>
 <session type="stop" time="1132522555" medium="AIM" to="RemoteBuddy123" from="LocalUser456"/>
-""".lstrip()
-        with self.assertRaises(NotImplementedError):
-            parser.Parser().parse(log_contents)
+""".lstrip()),
+            models.History(sessions=[
+                models.Session(messages=[
+                    models.Message(
+                        sender=u'RemoteBuddy123',
+                        timestamp=datetime.datetime(2005, 11, 20, 20, 7, 57),
+                        contents=u'what\'s up'),
+                    models.Message(
+                        sender=u'LocalUser456',
+                        timestamp=datetime.datetime(2005, 11, 20, 21, 35, 24),
+                        contents=u'nm')
+                ])
+            ]))
