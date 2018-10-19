534717d2338a864ac3a5214063817d667486f25d
Michael Lynch <git@mtlynch.io>
2018-10-19 12:19:39 -0500

    Adding support for Trillian session stop lines

--- chat_unifier/parsers/trillian/line_parser.py
+++ chat_unifier/parsers/trillian/line_parser.py
@@ -6,17 +6,39 @@ SessionStartLine = collections.namedtuple(
     'SessionStartLine',
     field_names=['timestamp', 'medium', 'sender', 'recipient'])
 
+SessionStopLine = collections.namedtuple(
+    'SessionStopLine',
+    field_names=['timestamp', 'medium', 'sender', 'recipient'])
+
 
 def parse(line):
     document = minidom.parseString(line)
-    return _parse_session_start(dict(document.childNodes[0].attributes.items()))
+    return _parse_session_attributes(
+        dict(document.childNodes[0].attributes.items()))
+
+
+def _parse_session_attributes(attributes):
+    if attributes[u'type'] == u'start':
+        return _parse_session_start(attributes)
+    else:
+        return _parse_session_stop(attributes)
 
 
 def _parse_session_start(attributes):
-    timestamp = datetime.datetime.utcfromtimestamp(int(attributes[u'time']))
-
     return SessionStartLine(
-        timestamp=timestamp,
+        timestamp=_parse_timestamp_attribute(attributes[u'time']),
         medium=attributes[u'medium'],
         sender=attributes[u'from'],
         recipient=attributes[u'to'])
+
+
+def _parse_session_stop(attributes):
+    return SessionStopLine(
+        timestamp=_parse_timestamp_attribute(attributes[u'time']),
+        medium=attributes[u'medium'],
+        sender=attributes[u'from'],
+        recipient=attributes[u'to'])
+
+
+def _parse_timestamp_attribute(timestamp_attribute):
+    return datetime.datetime.utcfromtimestamp(int(timestamp_attribute))
