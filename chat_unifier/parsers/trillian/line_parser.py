99dd503afd4e343bbc7535886a92704b9585ae0f
Michael Lynch <git@mtlynch.io>
2018-10-19 13:38:02 -0500

    Adding support for outgoing messages

--- chat_unifier/parsers/trillian/line_parser.py
+++ chat_unifier/parsers/trillian/line_parser.py
@@ -1,5 +1,6 @@
 import collections
 import datetime
+import urllib
 from xml.dom import minidom
 
 SessionStartLine = collections.namedtuple(
@@ -10,11 +11,23 @@ SessionStopLine = collections.namedtuple(
     'SessionStopLine',
     field_names=['timestamp', 'medium', 'sender', 'recipient'])
 
+OutgoingPrivateMessageLine = collections.namedtuple(
+    'OutgoingPrivateMessageLine',
+    field_names=[
+        'timestamp', 'medium', 'sender', 'sender_display', 'recipient',
+        'contents'
+    ])
+
 
 def parse(line):
     document = minidom.parseString(line)
-    return _parse_session_attributes(
-        dict(document.childNodes[0].attributes.items()))
+    node = document.childNodes[0]
+    tag_name = node.tagName
+    attributes = dict(node.attributes.items())
+    if tag_name == 'session':
+        return _parse_session_attributes(attributes)
+    elif tag_name == 'message':
+        return _parse_message_attributes(attributes)
 
 
 def _parse_session_attributes(attributes):
@@ -38,6 +51,31 @@ def _parse_session_stop(attributes):
         medium=attributes[u'medium'],
         sender=attributes[u'from'],
         recipient=attributes[u'to'])
+
+
+def _parse_message_attributes(attributes):
+    if attributes[u'type'] == u'outgoing_privateMessage':
+        return _parse_outgoing_message(attributes)
+    else:
+        return _parse_incoming_message(attributes)
+
+
+def _parse_outgoing_message(attributes):
+    return OutgoingPrivateMessageLine(
+        timestamp=_parse_timestamp_attribute(attributes[u'time']),
+        medium=attributes[u'medium'],
+        sender=attributes[u'from'],
+        sender_display=attributes[u'from_display'],
+        recipient=attributes[u'to'],
+        contents=_decode_message_text(attributes[u'text']))
+
+
+def _parse_incoming_message(attributes):
+    raise NotImplementedError('Incoming messages not yet supported')
+
+
+def _decode_message_text(text):
+    return urllib.unquote(text)
 
 
 def _parse_timestamp_attribute(timestamp_attribute):
