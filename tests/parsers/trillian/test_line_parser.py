d7a5b55b650b3c6e787e202d1f9eaebe51a0e6e6
Michael Lynch <mtlynch@users.noreply.github.com>
2018-10-19 12:23:35 -0500

    Adding support for Trillian session stop lines (#6)

--- tests/parsers/trillian/test_line_parser.py
+++ tests/parsers/trillian/test_line_parser.py
@@ -16,3 +16,14 @@ class LineParserTest(unittest.TestCase):
             line_parser.parse(
                 '<session type="start" time="1132517277" medium="AIM" to="RemoteBuddy123" from="LocalUser456"/>'
             ))
+
+    def test_parses_valid_session_stop_line(self):
+        self.assertEqual(
+            line_parser.SessionStopLine(
+                timestamp=datetime.datetime(2005, 11, 20, 21, 35, 55),
+                medium='AIM',
+                sender='LocalUser789',
+                recipient='RemoteBuddy234'),
+            line_parser.parse(
+                '<session type="stop" time="1132522555" medium="AIM" to="RemoteBuddy234" from="LocalUser789"/>'
+            ))
