79df12a98b3b9371088347052e982bd657bb208a
Michael Lynch <git@mtlynch.io>
2018-10-21 19:20:44 -0500

    Changing to JSON output

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python2
 
 import argparse
+import json
 import os
 import logging
 
-from chat_unifier import debug_print
+from chat_unifier import json_serializer
 from chat_unifier import history_merger
 from chat_unifier.parsers.trillian import parser as trillian_parser
 
@@ -30,13 +31,17 @@ def main(args):
         for filename in os.listdir(log_dir):
             if not filename.endswith('xml'):
                 continue
+            if filename.endswith('-assets.xml'):
+                continue
             full_path = os.path.join(log_dir, filename)
             with open(full_path) as log_handle:
                 parser = trillian_parser.Parser()
                 merger.add(parser.parse(log_handle.read()))
                 logger.info('Parsed %s', filename)
-    for history in merger:
-        debug_print.print_history(history)
+    print json.dumps([h for h in merger],
+                     indent=2,
+                     sort_keys=True,
+                     cls=json_serializer.Serializer)
 
 
 if __name__ == '__main__':
