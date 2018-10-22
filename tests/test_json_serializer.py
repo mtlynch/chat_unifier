087c67a5f89ccd952118488ce98a819d0f7e2492
Michael Lynch <mtlynch@users.noreply.github.com>
2018-10-22 06:37:26 -0700

    Adding a JSON serializer (#22)

---
+++ tests/test_json_serializer.py
@@ -0,0 +1,71 @@
+import datetime
+import json
+import unittest
+
+from chat_unifier import json_serializer
+from chat_unifier import models
+
+
+class HistoryTest(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_serializes_simple_history(self):
+        self.assertMultiLineEqual(
+            """
+[
+  {
+    "localUsername": "dummy_local123", 
+    "messages": [
+      {
+        "contents": "hi how are you?", 
+        "sender": "dummy_local123", 
+        "timestamp": "2018-10-18T18:27:05Z"
+      }
+    ], 
+    "remoteUsernames": [
+      "dummy_remote345"
+    ]
+  }, 
+  {
+    "localUsername": "dummy_local123", 
+    "messages": [
+      {
+        "contents": "hello good sir", 
+        "sender": "dummy_remote456", 
+        "timestamp": "2018-10-20T04:15:43Z"
+      }
+    ], 
+    "remoteUsernames": [
+      "dummy_remote456"
+    ]
+  }
+]
+""".strip(),
+            json.dumps(
+                [
+                    models.History(
+                        local_username='dummy_local123',
+                        remote_usernames=['dummy_remote345'],
+                        messages=[
+                            models.Message(
+                                sender='dummy_local123',
+                                timestamp=datetime.datetime(
+                                    2018, 10, 18, 18, 27, 5),
+                                contents='hi how are you?')
+                        ]),
+                    models.History(
+                        local_username='dummy_local123',
+                        remote_usernames=['dummy_remote456'],
+                        messages=[
+                            models.Message(
+                                sender='dummy_remote456',
+                                timestamp=datetime.datetime(
+                                    2018, 10, 20, 4, 15, 43),
+                                contents='hello good sir')
+                        ])
+                ],
+                sort_keys=True,
+                indent=2,
+                cls=json_serializer.Serializer))
