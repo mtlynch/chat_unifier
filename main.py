fdd430e9e326b8278d02afdd45f232a9c0993c7e
Michael Lynch <git@mtlynch.io>
2018-10-27 01:47:07 -0400

    Switching to trillian XML iterator

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import logging
 from chat_unifier import json_serializer
 from chat_unifier import history_merger
 from chat_unifier.parsers.trillian import parser as trillian_parser
+from chat_unifier.file_iterators import trillian_xml as trillian_xml_iterator
 
 logger = logging.getLogger(__name__)
 
@@ -28,20 +29,17 @@ def main(args):
     logger.info('Started runnning')
     merger = history_merger.Merger()
     for log_dir in args.log_dirs:
-        for filename in os.listdir(log_dir):
-            if not filename.endswith('xml'):
-                continue
-            if filename.endswith('-assets.xml'):
-                continue
-            full_path = os.path.join(log_dir, filename)
-            with open(full_path) as log_handle:
+        logger.info('Searching for logs in %s', log_dir)
+        for log_path in trillian_xml_iterator.iterate_files(log_dir):
+            with open(log_path) as log_handle:
                 parser = trillian_parser.Parser()
                 merger.add(parser.parse(log_handle.read()))
-                logger.info('Parsed %s', filename)
-    print json.dumps([h for h in merger],
-                     indent=2,
-                     sort_keys=True,
-                     cls=json_serializer.Serializer)
+                logger.info('Parsed %s', os.path.basename(log_path))
+    print json.dumps(
+        [h for h in merger],
+        indent=2,
+        sort_keys=True,
+        cls=json_serializer.Serializer)
 
 
 if __name__ == '__main__':
